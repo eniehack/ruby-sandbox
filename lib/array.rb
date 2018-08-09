b98cdc99a506be8adbbb182639d472394197ccd9
caffeine <eniehack@outlook.jp>
2018-08-09 16:36:49 +0900

    配列の作成を纏めた

--- lib/array.rb
+++ lib/array.rb
@@ -1,5 +1,16 @@
 nums = [1, 2, 3, 4, 5] #配列の定義
 p nums
 
-empty = [] #空の配列を作成
+empty = []
+=begin
+空の配列を作成
+empty = Array.newでも作成可能
+=end
 p empty
+
+array = Array.new(5, "Hello")
+# 全く同じオブジェクトが5つ作られる
+p array
+
+array = Array.new(5) {|index| index}
+# 全く違うオブジェクトが5つ作られる
